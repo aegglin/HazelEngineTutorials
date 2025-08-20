bbeb28b59e7c6ee040ae63bd4fc85679cda1faa0
Aiden Egglin <uefadude@gmail.com>
2025-08-20 08:01:23 -0400

    Add precompiled headers and start window generatipon

---
+++ Hazel/src/Hazel/Window.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "hzpch.h"
+
+#include "Hazel/Core.h"
+#include "Hazel/Events/Event.h"
+
+namespace Hazel {
+	struct WindowProps
+	{
+		std::string Title;
+		unsigned int Width;
+		unsigned int Height;
+
+		WindowProps(const std::string& title = "Hazel Engine",
+			unsigned int width = 1280,
+			unsigned int height = 720)
+			:Title(title), Width(width), Height(height) {}
+	};
+
+
+	// Interface representing a desktop system based Window (all pure virtual functions)
+	// This will be implemented by windows on each OS (Windows, mac, linux)
+	class HAZEL_API Window
+	{
+	public:
+		using EventCallbackFn = std::function<void(Event&)>;
+		virtual ~Window();
+		virtual void OnUpdate() = 0;
+		virtual unsigned int GetWidth() const = 0;
+		virtual unsigned int GetHeight() const = 0;
+
+		//window attributes
+		virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
+		virtual void SetVSync(bool enabled) = 0;
+
+		//Must be implemented per platform
+		static Window* Create(const WindowProps& props = WindowProps());
+
+	};
+}
