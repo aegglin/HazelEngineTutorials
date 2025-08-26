8546ca77b2662045de77a3f907fbc4c0251543dd
Aiden Egglin <uefadude@gmail.com>
2025-08-25 22:03:29 -0400

    Update with window creation

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -7,15 +7,24 @@
 
 namespace Hazel
 {
+
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		// Don't need to delete the window ourselves when the app terminates
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		HZ_CORE_INFO("{0}", e);
 	}
 
 	void Application::Run()
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -14,6 +14,7 @@ namespace Hazel
 
 		void Run();
 
+		void OnEvent(Event& e);
 	private:
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
